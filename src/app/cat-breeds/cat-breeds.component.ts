ecedc64f7f4e61528bd8f95943ac113e9c8ced1a
Angel Dixon <angel.can.work@gmail.com>
2025-03-15 00:49:46 -0400

    Updates styles and filter

--- src/app/cat-breeds/cat-breeds.component.ts
+++ src/app/cat-breeds/cat-breeds.component.ts
@@ -1,30 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { CatsService } from '../cats.service';
+import { CatBreedDetails } from '../cats.model';
 
 @Component({
   selector: 'app-cat-breeds',
   templateUrl: './cat-breeds.component.html',
-  styleUrls: ['./cat-breeds.component.scss']
+  styleUrls: ['./cat-breeds.component.scss'],
 })
 export class CatBreedsComponent implements OnInit {
-  private catBreeds: any[] = [];
+  private catBreeds: CatBreedDetails[] = [];
 
   searchTerm: string = '';
-  displayBreeds: any[] = [];
+  displayBreeds: CatBreedDetails[] = [];
   catBreedsLoaded: boolean = false;
   filters = [
     {
-      name: 'hypoallergenic', isSelected: true
+      name: 'hypoallergenic',
+      isSelected: false,
     },
     {
-      name: 'rare', isSelected: true
+      name: 'rare',
+      isSelected: false,
     },
     {
-      name: 'hairless', isSelected: true
-    }
+      name: 'hairless',
+      isSelected: false,
+    },
   ];
 
-  constructor(private catService: CatsService) { }
+  constructor(private catService: CatsService) {}
 
   ngOnInit(): void {
     this.getCatBreeds();
@@ -32,19 +36,41 @@ export class CatBreedsComponent implements OnInit {
 
   getCatBreeds(filterBreeds?: boolean) {
     this.catBreedsLoaded = false;
-    this.catService.getCatBreeds().subscribe(
-      data => {
-        this.catBreeds = data;
-        this.displayBreeds = data;
-        if (filterBreeds) this.filterBreeds();
-        this.catBreedsLoaded = true;
-      });
+    this.catService.getCatBreeds().subscribe((data) => {
+      this.catBreeds = this.mapBreeds(data);
+      this.displayBreeds = this.catBreeds;
+      if (filterBreeds) this.filterBreeds();
+      this.catBreedsLoaded = true;
+    });
   }
 
   filterBreeds() {
-    this.displayBreeds = this.catBreeds;
-    const selectedFilters = this.filters.filter(filter => !filter.isSelected);
-    // change filter to only show ones from selected
-    selectedFilters.forEach(filter => this.displayBreeds = this.displayBreeds.filter(breed => breed[filter.name] === 0));
+    const selectedFilters = this.filters
+      .filter((filter) => filter.isSelected)
+      .map((filter) => filter.name);
+
+    if(selectedFilters.length > 0) {
+      selectedFilters.forEach((filter: string) => {
+        this.displayBreeds = this.catBreeds.filter((breed: any) => breed[filter]);
+      });
+    } else {
+      this.displayBreeds = this.catBreeds;
+    }
+
+
+  }
+
+  private mapBreeds(data: any): CatBreedDetails[] {
+    return data.map((cat: any) => {
+      return {
+        id: cat.id,
+        name: cat.name,
+        privacy: cat.privacy,
+        imageUrl: cat.image,
+        hypoallergenic: cat.hypoallergenic,
+        rare: cat.rare,
+        hairless: cat.hairless,
+      };
+    });
   }
 }
---
+++ src/app/cats.model.ts
@@ -0,0 +1,9 @@
+export type CatBreedDetails = {
+  id: string,
+  name: string,
+  privacy: string,
+  imageUrl: string,
+  hypoallergenic: boolean,
+  rare: boolean,
+  hairless: boolean
+}
